2da5cb3ce7ce79bf64960346aa98dfb721031aa0
MohamadRacer <126101878+MohamadRacer@users.noreply.github.com>
2023-08-23 14:51:32 +0330

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,6 @@
 # Imports 
 import tensorflow as tf 
-import numpy as np
+import numpy
 import pandas as pd 
 import requests
 import matplolib.pyplot as plt
@@ -15,11 +15,11 @@ def plot_decision_boundary(model, X, y):
   # Define the axis boundaries of the plot and create a meshgrid
   x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
   y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
-  xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
-                       np.linspace(y_min, y_max, 100))
+  xx, yy = numpy.meshgrid(numpy.linspace(x_min, x_max, 100),
+                       numpy.linspace(y_min, y_max, 100))
   
   # Create X value (we're going to make predictions on these)
-  x_in = np.c_[xx.ravel(), yy.ravel()] # stack 2D arrays together
+  x_in = numpy.c_[xx.ravel(), yy.ravel()] # stack 2D arrays together
 
   # Make predictions
   y_pred = model.predict(x_in)
@@ -28,10 +28,10 @@ def plot_decision_boundary(model, X, y):
   if len(y_pred[0]) > 1:
     print("doing multiclass classification")
     # We have to reshape our prediction to get them ready for plotting
-    y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+    y_pred = numpy.argmax(y_pred, axis=1).reshape(xx.shape)
   else:
     print("doing binary classification")
-    y_pred = np.round(y_pred).reshape(xx.shape)
+    y_pred = numpy.round(y_pred).reshape(xx.shape)
   
   # Plot the decision boundary
   plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
