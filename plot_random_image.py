805ea992d3ff7617775372f5c4d8448f35bd1a15
MohamadRacer <126101878+MohamadRacer@users.noreply.github.com>
2023-08-27 14:56:18 +0330

    first commit

---
+++ plot_random_image.py
@@ -0,0 +1,29 @@
+import random
+
+def plot_random_image(model, images, true_labels, classes):
+  """
+  Picks a random image, plots it and labels it with a prediction and truth label.
+  """
+  # Set up random integer
+  i = random.randint(0, len(images))
+
+  # Create predictions and targets
+  target_image = images[i]
+  pred_probs = model.predict(target_image.reshape(1, 28, 28))
+  pred_label = classes[pred_probs.argmax()]
+  true_label = classes[true_labels[i]]
+
+  # Plot the image
+  plt.imshow(target_image, cmap=plt.cm.binary)
+
+  # Change the color of the titles depending on if the prediction is right or wrong
+  if pred_label == true_label:
+    color = "green"
+  else:
+    color = "red"
+  
+  # Add xlabel information (prediction/true label)
+  plt.xlabel("Pred: {} {:2.0f}% (True: {})".format(pred_label,
+                                                   100*tf.reduce_max(pred_probs),
+                                                   true_label),
+             color=color) # set the color to green or red based on if prediction is right or wrong
